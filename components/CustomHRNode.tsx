12d13056fa2afc3723740fe5bc3641e5c02de3ab
wonoong <dnd5301@gmail.com>
2025-06-04 05:18:27 +0900

    FEAT: Add Color Change

---
+++ components/CustomHRNode.tsx
@@ -0,0 +1,53 @@
+import { DecoratorNode, NodeKey, LexicalNode } from 'lexical';
+import React from 'react';
+
+export class CustomHRNode extends DecoratorNode<React.ReactElement> {
+  __style: string;
+
+  static getType(): string {
+    return 'custom-hr';
+  }
+
+  static clone(node: CustomHRNode): CustomHRNode {
+    return new CustomHRNode(node.__style, node.__key);
+  }
+
+  constructor(style: string, key?: NodeKey) {
+    super(key);
+    this.__style = style;
+  }
+
+  createDOM(): HTMLElement {
+    const div = document.createElement('div');
+    div.style.userSelect = 'none';
+    return div;
+  }
+
+  updateDOM(): false {
+    return false;
+  }
+
+  decorate(): React.ReactElement {
+    return <hr className={this.__style} />;
+  }
+
+  isIsolated(): boolean {
+    return true;
+  }
+
+  canBeEmpty(): boolean {
+    return false;
+  }
+
+  canInsertTextBefore(): boolean {
+    return true;
+  }
+
+  canInsertTextAfter(): boolean {
+    return true;
+  }
+}
+
+export function $createCustomHRNode(style: string): CustomHRNode {
+  return new CustomHRNode(style);
+} 
