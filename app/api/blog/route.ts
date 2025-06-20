de1d6e84ef6053ceac70f050a1a34d227aadd551
ChoiWonOong <dnd5301@gmail.com>
2025-06-20 15:36:04 +0900

    FIX: Fix Environment Variables

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ error: 'address 또는 id 파라미터가 필요합니다.' }, { status: 400 });
   }
 
-  const springUrl = address ? process.env.NEXT_PUBLIC_API_URL + `/api/blog/address/${address}` : process.env.NEXT_PUBLIC_API_URL + `/api/blog/${id}`;
+  const springUrl = address ? process.env.NEXT_PUBLIC_API_SERVER + `/api/blog/address/${address}` : process.env.NEXT_PUBLIC_API_SERVER + `/api/blog/${id}`;
 
   try {
     const res = await fetch(springUrl);
